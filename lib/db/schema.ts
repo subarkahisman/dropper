dfdfc98ae23ac49a8eb8378e38dfe77747ed719f
subarkahisman <ismansubarkah.2210@gmail.com>
2025-05-08 22:43:42 +0700

    create File model by Drizzle ORM

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,2 +1,37 @@
-import { pgTable, text, uuid, integer, boolean } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  text,
+  uuid,
+  integer,
+  boolean,
+  timestamp,
+} from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
+
+export const files = pgTable("files", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: text("name").notNull(),
+  path: text("path").notNull(),
+  size: integer("size").notNull(),
+  type: text("type").notNull(),
+  fileUrl: text("file_url").notNull(),
+  thumbnailUrl: text("thumbnail_url"),
+  userId: text("user_id").notNull(),
+  parentId: uuid("parent_id"),
+  isFolder: boolean("is_folder").default(false).notNull(),
+  isStarred: boolean("is_starred").default(false).notNull(),
+  isTrash: boolean("is_trash").default(false).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("update_at").defaultNow().notNull(),
+});
+
+export const filesRelations = relations(files, ({ one, many }) => ({
+  parent: one(files, {
+    fields: [files.parentId],
+    references: [files.id],
+  }),
+  children: many(files),
+}));
+
+export const File = typeof files.$inferSelect;
+export const NewFile = typeof files.$inferInsert;
